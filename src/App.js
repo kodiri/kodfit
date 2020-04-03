13acb1088ff573759af59d1a7832983edc5724b3
Marian Molocea <marian.mcrt@gmail.com>
2020-04-03 20:43:16 +0100

    Create bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import BottomNavBar from './BottomNavBar/BottomNavBar';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Welcome to Kodfit!!!</h1>
-    </div>
+    <Router>
+      <div className="App">
+          <h1>Welcome to Kodfit!!!</h1>
+          <BottomNavBar />
+      </div>
+    </Router>
   );
 }
 
