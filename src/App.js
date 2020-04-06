619757fc9b978f7eb48baca6a2a7f52519d5ea5b
Marian Molocea <marian.mcrt@gmail.com>
2020-04-06 11:27:30 +0100

    Add general styling box-sizing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BottomNavBar from './BottomNavBar/BottomNavBar';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import CalendarApp from './CalendarApp/CalendarApp';
 
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <div className="App">
-          <h1>Welcome to Kodfit!!!</h1>
-          <Route path="/calendar" exact component={CalendarApp} />
-          <BottomNavBar />
+        <h1>Welcome to Kodfit!!!</h1>
+        <Route path="/calendar" exact component={CalendarApp} />
+        <BottomNavBar />
       </div>
     </Router>
   );
