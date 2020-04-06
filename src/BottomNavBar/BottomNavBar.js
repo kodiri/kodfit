8218b30d31a09d2584d4df26a110bf7c62a54ce0
Marian Molocea <marian.mcrt@gmail.com>
2020-04-06 21:29:34 +0100

    Create a new data structure for the button and iterate through

--- src/BottomNavBar/BottomNavBar.js
+++ src/BottomNavBar/BottomNavBar.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import NavBarButton from './BottomNavBarButton/BottomNavBarButton'
 import './BottomNavBar.css'
+import {FaDumbbell, FaCalendarAlt} from 'react-icons/fa'
+import {MdInsertChart, MdAccountCircle, MdLocationOn} from 'react-icons/md'
 
 
 export default function BottomNavBar() {
-  const navLinks = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6'];
+  const navLinks = [
+    {name: 'schedule', path: 'calendar', icon: <FaCalendarAlt />},
+    {name: 'nearby', path: 'near-me', icon: <MdLocationOn />},
+    {name: 'workout', path: 'workout-main', icon: <FaDumbbell />},
+    {name: 'progress', path: 'tracker', icon: <MdInsertChart />},
+    {name: 'profile', path: 'workout-current', icon: <MdAccountCircle />},
+  ]
 
   return (
     <div className="BottomNavBar">
-      {navLinks.map(navLink => <NavBarButton key={navLink} link={navLink} />)}
+      {
+        navLinks.map(navLink => (
+          <NavBarButton 
+            key={navLink.name}
+            name={navLink.name}
+            link={navLink.path} 
+            icon={navLink.icon}
+          />
+        ))
+      }
     </div>
   )
 }
--- src/BottomNavBar/BottomNavBarButton/BottomNavBarButton.js
+++ src/BottomNavBar/BottomNavBarButton/BottomNavBarButton.js
@@ -4,11 +4,13 @@ import './BottomNavBarButton.css'
 import { FaDumbbell } from 'react-icons/fa'
 
 
-export default function BottomNavBarButton({ link }) {
+export default function BottomNavBarButton({ link, name, icon }) {
   return (
     <NavLink to={`/${link}`} className="BottomNavBarButton" activeClassName='active'>
-      <FaDumbbell />
-      { link }
+      <div className="icon">
+        { icon }
+      </div>
+      { name.toUpperCase() }
     </NavLink>
   )
 }
