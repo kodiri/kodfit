e17b714832ca6af556e28c929ed65404f64be5c4
casey-minzi <cluvhope@gmail.com>
2020-04-06 17:46:08 +0100

    Added Route / deleted App.css text align:center

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BottomNavBar from './BottomNavBar/BottomNavBar';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import CalendarApp from './CalendarApp/CalendarApp';
 import NearMeGyms from './NearMe/NearMeGyms/NearMeGyms';
