194d79a44d623553481c2a4e58ce7da85e64b53c
Marian Molocea <marian.mcrt@gmail.com>
2020-04-07 14:51:46 +0100

    Resturcture the pages into Page view Component

--- src/PageView/NavBar/NavBarButton/NavBarButton.js
+++ src/PageView/NavBar/NavBarButton/NavBarButton.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './NavBarButton.css'
 
-
 export default function NavBarButton({ link, name, icon }) {
   return (
     <NavLink to={`/${link}`} className="NavBarButton" activeClassName='active'>
