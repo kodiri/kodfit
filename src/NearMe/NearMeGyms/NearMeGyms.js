6091ecf9b30595f44d1619d036da375f57bf460e
Marian Molocea <marian.mcrt@gmail.com>
2020-04-07 20:52:01 +0100

    Add google map and get the location from the user to load the map at user's address

--- src/NearMe/NearMeGyms/NearMeGyms.js
+++ src/NearMe/NearMeGyms/NearMeGyms.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import './NearMeGyms.css';
 
 export default function NearMeGyms() {
-    return (
-        <div className="NearMeGyms">
-            <div className="containerOne">
-                <div className="map containerOne-content">
-                    this will be map
-                </div>
-                <div className="list containerOne-content">
-                    <h3 className="listTitle">List of Gyms</h3>
-                    <ul className="listContent">
-                        <li className="listContent-item">gym A</li>
-                        <li className="listContent-item">gym B</li>
-                        <li className="listContent-item">gym C</li>
-                        <li className="listContent-item">gym D</li>
-                    </ul>
-                </div>
-            </div>
+
+  let [location, setLocation] = useState('');
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords}) => {
+      setLocation({
+        lat: coords.latitude,
+        lng: coords.longitude
+      })
+    })
+  }, [location])
+
+  return (
+    <div className="NearMeGyms">
+      <div className="containerOne">
+        <div className="map containerOne-content">
+          <LoadScript 
+            id='load-script'
+            googleMapsApiKey="AIzaSyDJafHKT_k97cPZ826in74GF-yIV_ww7Hk"
+          >
+            <GoogleMap 
+              id='map'
+              zoom={13}
+              center={location || {lat: 51.5007, lng: -0.1246}}
+              mapContainerClassName='map'
+            />
+          </LoadScript>
         </div>
-    )
+        
+        <div className="list containerOne-content">
+          <h3 className="listTitle">List of Gyms</h3>
+          <ul className="listContent">
+            <li className="listContent-item">gym A</li>
+            <li className="listContent-item">gym B</li>
+            <li className="listContent-item">gym C</li>
+            <li className="listContent-item">gym D</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
