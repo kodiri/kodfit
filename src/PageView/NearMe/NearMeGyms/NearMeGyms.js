0fd89676e2b99b54d56f32a89059ce9a3d35dd25
Marian Molocea <marian.mcrt@gmail.com>
2020-04-08 14:22:24 +0100

    Try to fetch data from google API

--- src/PageView/NearMe/NearMeGyms/NearMeGyms.js
+++ src/PageView/NearMe/NearMeGyms/NearMeGyms.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerClusterer, Marker } from '@react-google-maps/api';
 import './NearMeGyms.css';
 
 export default function NearMeGyms() {
 
-  let [location, setLocation] = useState('');
+  let [location, setLocation] = useState({lat: 51.5007, lng: -0.1246});
+  let [results, setResults] = useState([])
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({coords}) => {
       setLocation({
         lat: coords.latitude,
         lng: coords.longitude
       })
-    })
+    });
+/*       const response = await fetch(`https://maps.googleapis.coma/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=1500&type=gym&key=AIzaSyDJafHKT_k97cPZ826in74GF-yIV_ww7Hk`);
+      const results = await response.json();
+      setResults([results]); */
   }, [location])
 
   return (
@@ -20,14 +24,18 @@ export default function NearMeGyms() {
         <div className="map containerOne-content">
           <LoadScript 
             id='load-script'
-            googleMapsApiKey="AIzaSyDJafHKT_k97cPZ826in74GF-yIV_ww7Hk"
+            googleMapsApiKey=""
           >
             <GoogleMap 
               id='map'
               zoom={13}
-              center={location || {lat: 51.5007, lng: -0.1246}}
+              center={location}
               mapContainerClassName='map'
-            />
+            >
+              <MarkerClusterer >
+                <Marker />
+              </MarkerClusterer>
+            </GoogleMap>
           </LoadScript>
         </div>
         
