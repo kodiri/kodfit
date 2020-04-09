a5a3bd7d27cc27a5d07169c284bbd576bc6302a8
casey-minzi <cluvhope@gmail.com>
2020-04-09 23:24:10 +0100

    testing google login

--- src/PageView/Login/Login.js
+++ src/PageView/Login/Login.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './Login.css';
 import InputBox from './InputBox/InputBox';
 import kodfit_logo from './../../common/images/kodfit_logo_large.svg';
+import GoogleLogin from 'react-google-login';
 
 export default function Login() {
+    const responseGoogle = (response) => {
+        console.log(response);
+      }      
+
     return (
         <div className="Login">
             <div className="logoBox">
@@ -17,6 +22,21 @@ export default function Login() {
                 <div className="loginButtons">
                     <button className="signInButton"> Sign In</button>
                     <button className="signUpButton"> Sign Up</button>
+                    <GoogleLogin
+                        clientId="848040990578-mpvcvg31e99180qb7f28rqvfm708iqp3.apps.googleusercontent.com"
+                        render={renderProps => (
+                            <button
+                                onClick={renderProps.onClick}
+                                disabled={renderProps.disabled}
+                                className="btn-google">
+                                    Google Sign In
+                                </button>
+                          )}
+                          buttonText="Login"
+                          onSuccess={responseGoogle}
+                          onFailure={responseGoogle}
+                          cookiePolicy={'single_host_origin'}
+                        />,
                 </div>
             </main>
         </div>
