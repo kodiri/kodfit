78ce9854ff62deca803cd632b0b16a0889a4f3cc
Marian Molocea <marian.mcrt@gmail.com>
2020-04-10 15:35:49 +0100

    Introduce Switch toggle buttons and redenr the list of gyms when button is active

--- src/PageView/NearMe/NearMeGyms/NearMeGyms.js
+++ src/PageView/NearMe/NearMeGyms/NearMeGyms.js
@@ -6,11 +6,13 @@ import {
 } from '@react-google-maps/api';
 import './NearMeGyms.css';
 import InfoWindows from './InfoWindows/InfoWindows'
+import Switch from 'react-switch';
 
 export default function NearMeGyms() {
-  const [data, setData] = useState([]);
+  const [gymsData, setGymsData] = useState([]);
   const [location, setLocation] = useState({ lat: 51.5007, lng: -0.1246 });
   const [markerId, setMarkerId] = useState('')
+  const [markerQuery, setMarkerQuery] = useState({gyms: true, trainers: false})
   useEffect(() => {
     let successPosition = (position) => {
       setLocation({
@@ -28,7 +30,7 @@ export default function NearMeGyms() {
         `https://discover.search.hereapi.com/v1/discover?at=${location.lat},${location.lng}&q=gym&limit=15&apiKey=${process.env.REACT_APP_HERE_API}`
       );
       const result = await response.json();
-      setData(result.items);
+      setGymsData(result.items);
     })();
   }, [location]);
 
@@ -43,28 +45,66 @@ export default function NearMeGyms() {
               center={location}
               mapContainerClassName="map"
             >
-              {data &&
-                data.map((place) => (
-                  <Marker
-                    key={place.id}
-                    position={place.position}
-                    onClick={() => setMarkerId(place.id)}>
-                      {
-                        markerId === place.id ? <InfoWindows place={place} /> : ''
-                      }                                         
-                    </Marker>
-                ))}
+              { markerQuery.gyms ?
+                (gymsData &&
+                  gymsData.map((place) => (
+                    <Marker
+                      key={place.id}
+                      position={place.position}
+                      onClick={() => setMarkerId(place.id)}>
+                        {
+                          markerId === place.id ? <InfoWindows place={place} /> : ''
+                        }                                         
+                      </Marker>
+                  ))) :
+                ''
+              }
             </GoogleMap>
           </LoadScript>
         </div>
-
+        <div className="switch-buttons containerOne-content">
+          <label>
+            Show gyms nearby
+            <Switch 
+              onChange={() => setMarkerQuery({gyms: !markerQuery.gyms, trainers: markerQuery.trainers})} 
+              checked={markerQuery.gyms}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              height={20}
+              width={48}
+              onColor='#07a807'
+              handleDiameter={25}
+              boxShadow="1px 1px 5px rgba(0, 0, 0, 0.6)"
+              activeBoxShadow="-1px 1px 1px 10px rgba(0, 0, 0, 0.2)"
+            />
+          </label>
+          <label>
+            Show personal trainers nearby
+            <Switch 
+              onChange={() => setMarkerQuery({gyms: markerQuery.gyms, trainers: !markerQuery.trainers})} 
+              checked={markerQuery.trainers}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              height={20}
+              width={48}
+              onColor='#07a807'
+              handleDiameter={25}
+              boxShadow="1px 1px 5px rgba(0, 0, 0, 0.6)"
+              activeBoxShadow="-1px 1px 1px 10px rgba(0, 0, 0, 0.2)"
+            />
+          </label>
+        </div>
         <div className="list containerOne-content">
           <h3 className="listTitle">List of Gyms</h3>
           <ul className="listContent">
-            <li className="listContent-item">gym A</li>
-            <li className="listContent-item">gym B</li>
-            <li className="listContent-item">gym C</li>
-            <li className="listContent-item">gym D</li>
+            {
+              markerQuery.gyms ?
+                (gymsData &&
+                  gymsData.map((place) => (
+                    <li key={place.id+place.title} className="listContent-item">{place.title}</li>
+                  ))
+                ) : ''
+            }
           </ul>
         </div>
       </div>
