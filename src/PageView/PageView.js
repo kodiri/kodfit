e13a7c35e51a714cd1c41d79ef768891f6a76f94
Marian Molocea <marian.mcrt@gmail.com>
2020-04-13 22:42:48 +0100

    change the roots to link all the components

--- src/PageView/PageView.js
+++ src/PageView/PageView.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CalendarApp from './CalendarApp/CalendarApp';
 import NearMeGyms from './NearMe/NearMeGyms/NearMeGyms';
-import NearMeTrainers from './NearMe/NearMeTrainers/NearMeTrainers';
 import WorkoutsMain from './WorkoutsMain/WorkoutsMain';
 import WorkoutList from './WorkoutsMain/WorkoutList/WorkoutList';
 import WorkoutCurrent from './WorkoutsMain/WorkoutCurrent/WorkoutCurrent';
-import NearMe from './NearMe/NearMe';
 import Tracker from './Tracker/Tracker';
 import Login from './Login/Login';
 import Timer from './Timer/Timer';
@@ -24,9 +22,7 @@ export default function PageView() {
       <Route path="/about" exact component={AboutUs} />
       <Route path="/our-goal" exact component={OurGoal} />
       <Route path="/dashboard/calendar" exact component={CalendarApp} />
-      <Route path="/dashboard/near-me" exact component={NearMe} />
       <Route path="/dashboard/near-me-gyms" exact component={NearMeGyms} />
-      <Route path="/dashboard/near-me-trainers" exact component={NearMeTrainers} />
       <Route path="/dashboard/home" exact component={WorkoutsMain} />
       <Route path="/dashboard/workout-list" exact component={WorkoutList} />
       <Route path="/dashboard/workout-current" exact component={WorkoutCurrent} />
