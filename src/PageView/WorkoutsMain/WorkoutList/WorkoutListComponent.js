e2af0bffa89d8e31e03d64b5205c7864227d9691
Daniel Arango <danielarango@daniels-air.lan>
2020-04-21 15:20:45 +0100

    commit pre merge with latest upstream

--- src/PageView/WorkoutsMain/WorkoutList/WorkoutListComponent.js
+++ src/PageView/WorkoutsMain/WorkoutList/WorkoutListComponent.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Link } from 'react';
 import './WorkoutListComponent.css';
 
 
 export default class WorkoutListComponent extends React.Component {
 
   render() {
-    let { workoutName, image, workoutAlt } = this.props;
+    let { id, workoutName, image, workoutAlt } = this.props;
 
     return (
+      <Link to={`/dashboard/workout-list/${id}`}>
           <div className="routine">
             <p className="text">{workoutName}</p>
             <img src={image} alt={workoutAlt}/>
           </div>
+      </Link>
     )
   }
 }
